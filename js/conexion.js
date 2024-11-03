ff2c2c3f3a74ba9205cc3618eb6e90b0b4081f79
DanielGarridoLuna <draymonluna@gmail.com>
2024-11-03 09:54:03 -0600

    login exitoso

---
+++ js/conexion.js
@@ -0,0 +1,9 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyBU8wvlPhkclNKz23I5x4KMLMgzW6dRp6k",
+    authDomain: "basededatos-6fc16.firebaseapp.com",
+    projectId: "basededatos-6fc16",
+    storageBucket: "basededatos-6fc16.firebasestorage.app",
+    messagingSenderId: "465190612892",
+    appId: "1:465190612892:web:4abb036fd9164f0514e259"
+});
